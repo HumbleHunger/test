7542d8e422bf1da1c582cf485df57834cce4c18e
HumbleHunger <2495970924@qq.com>
2020-12-07 20:56:04 +0800

    s

--- 5.c
+++ 5.c
@@ -1,22 +1,22 @@
 #include <stdio.h>
-void main()
+int main()
 {
-        int j,k;
-        int num[2];
-        while(scanf("%d %d",&num[0],&num[1])!=EOF){
-		k=0;
-                for ( j=num[0];j<=num[1];j++ ) {
-			int g,s,b;
+        int j;
+        int n;
+        scanf("%d",&n);
+        int num[2]={1,10};
+        for(int i=1;i<n;i++){
+            num[0]*=10;
+            num[1]*=10;
+        }
+        num[1]=num[1]-1;
+        for(j=num[0];j<=num[1];j++){
+		    int g,s,b;
 			g=j%10;
 			s=(int)j%100/10;
 			b=(int)j/100;
 			if(j==g*g*g+s*s*s+b*b*b){
-				if(k==0){printf("%d",j);}
-				else{printf(" %d",j);}
-				k++;
+				printf("%d\n",j);
 			}
-                }
-                if(k==0){printf("no");}
-		printf("\n");
-       }
+        }
 }
