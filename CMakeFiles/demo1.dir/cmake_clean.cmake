314fedf94e492c82d84855211d647e693d1ee4f1
HumbleHunger <2495970924@qq.com>
2021-03-23 22:37:42 +0800

    .

---
+++ CMakeFiles/demo1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/demo1.dir/1.cc.o"
+  "demo1"
+  "demo1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/demo1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
